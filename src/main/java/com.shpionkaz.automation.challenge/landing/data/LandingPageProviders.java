b722be41b2d74f4a9066d68b9e78b4c12663503a
shpionka <topchylo@gmail.com>
2016-06-17 00:47:33 +0200

    change locators order

--- src/main/java/com.shpionkaz.automation.challenge/landing/data/LandingPageProviders.java
+++ src/main/java/com.shpionkaz.automation.challenge/landing/data/LandingPageProviders.java
@@ -3,14 +3,17 @@ package com.shpionkaz.automation.challenge.landing.data;
 public interface LandingPageProviders {
 
     /**
-     * IDs
+     * ID
      */
     String DepartureInput = "from_filter";
     String ArrivalInput = "to_filter";
-    String AirbnbCheckbox = ".hotel-checkbox__airbnb";
-    String Body = "body";
-
     String SearchFormSubmitButton = "search-form__submit-btn";
+
+    /**
+     * CSS
+     */
+    String Body = "body";
+    String AirbnbCheckbox = ".hotel-checkbox__airbnb";
 
 
 }
