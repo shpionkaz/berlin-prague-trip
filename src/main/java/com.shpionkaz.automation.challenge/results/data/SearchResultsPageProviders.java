6e4e590049e3d92e16345cba8eedba67fac03349
shpionka <topchylo@gmail.com>
2016-06-17 00:50:45 +0200

    change Search page locators

--- src/main/java/com.shpionkaz.automation.challenge/results/data/SearchResultsPageProviders.java
+++ src/main/java/com.shpionkaz.automation.challenge/results/data/SearchResultsPageProviders.java
@@ -2,4 +2,13 @@ package com.shpionkaz.automation.challenge.results.data;
 
 
 public interface SearchResultsPageProviders {
+
+    /**
+     * ID
+     */
+    String TrainsTab = "tab_train";
+    String PlanesTab = "tab_flight";
+    String BusesTab = "tab_bus";
+    String PriceSortingFilter = "sortby-price";
+    String TravelTimeSortingFilter = "sortby-traveltime";
 }
